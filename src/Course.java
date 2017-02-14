1f802b5a771f122673a269d63ac0f1922987f3be
nagpa <pnagpal@uwo.ca>
2017-02-13 19:14:04 -0500

    Added Course class and made changes to Student and Instructor

---
+++ src/Course.java
@@ -0,0 +1,13 @@
+/**
+ * Created by nagpa on 2017-02-13.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description)
+    {
+        this.cname = cname;
+        this.description = description;
+    }
+}
